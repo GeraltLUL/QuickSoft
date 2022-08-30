7d0bcfced94e72e7676bee054e6063d6ced22310
Geralt <rasmygens@gmail.com>
2022-08-30 05:37:08 +0300

    secret keys

--- app.py
+++ app.py
@@ -9,113 +9,16 @@ import uuid
 from datetime import datetime
 
 
-class User(UserMixin):
-    id = ''
-    name = ''
-    email = ''
-    password = ''
-    registerDate = ''
-    avatar = ''
-    gameSessions = []
-
-    def __init__(self, id, name, email, password, registerDate, avatar, gameSessions):
-        self.id = id
-        self.name = name
-        self.email = email
-        self.password = password
-        self.registerDate = registerDate
-        self.avatar = avatar
-        self.gameSessions = gameSessions
-
-    def get_id(self):
-        return self.id
-
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-
-def create_record(data):
-    date_now = datetime.now()
-    now_month = date_now.month
-    now_day = date_now.day
-
-    if now_month <= 10:
-        now_month = f"0{now_month}"
-
-    if now_day <= 10:
-        now_day = f"0{now_day}"
-
-    collection.insert_one({
-        'id': str(uuid.uuid4()),
-        'name': data['name'],
-        'email': data['email'],
-        'password': data['password'],
-        'registerDate': f"{now_day}.{now_month}.{date_now.year}",
-        'avatar': 'user_tmp_example.png',
-        'gameSessions': []
-    })
-
-
-def find_user_by_id(user_id):
-    cur_user = collection.find_one({'id': user_id})
-    if cur_user is not None:
-        return User(id=cur_user['id'],
-                    name=cur_user['name'],
-                    email=cur_user['email'],
-                    password=cur_user['password'],
-                    registerDate=cur_user['registerDate'],
-                    avatar=cur_user['avatar'],
-                    gameSessions=cur_user['gameSessions'])
-    else:
-        return None
-
-
-def find_user_by_email(value):
-    cur_user = collection.find_one({'email': value})
-    if cur_user is not None:
-        return User(id=cur_user['id'],
-                    name=cur_user['name'],
-                    email=cur_user['email'],
-                    password=cur_user['password'],
-                    registerDate=cur_user['registerDate'],
-                    avatar=cur_user['avatar'],
-                    gameSessions=cur_user['gameSessions'])
-    else:
-        return None
-
-
-def delete_record_by_id(value):
-    collection.delete_one({
-        'id': value
-    })
-
-
-def update_record(findKey, findValue, key, value):
-    collection.find_one_and_update({findKey: findValue},
-                                   {'$set': {key: value}})
-
-
-def update_and_push(findKey, findValue, key, value):
-    collection.find_one_and_update({findKey: findValue},
-                                   {'$push': {key: value}})
-
-
 # Flask config
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '12s32gds32f1523das3df'
+app.config['SECRET_KEY'] = os.getenv('FLASK_KEY')
 app.config['JSON_AS_ASCII'] = False
 app.config['SECURITY_UNAUTHORIZED_VIEW'] = '/login'
 app.config['UPLOAD_IMAGE_FOLDER'] = './static/assets/img/usersAvatars'
 app.config.from_object(__name__)
 CORS(app)
 
-client = MongoClient('localhost', 27017)
+client = MongoClient(os.getenv('MONGODB_URI'))
 db = client['QuickSoft']
 collection = db['users']
 
@@ -306,6 +209,102 @@ def get_game_sessions():
     except Exception as e:
         print(e)
 
+
+class User(UserMixin):
+    id = ''
+    name = ''
+    email = ''
+    password = ''
+    registerDate = ''
+    avatar = ''
+    gameSessions = []
+
+    def __init__(self, id, name, email, password, registerDate, avatar, gameSessions):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+        self.registerDate = registerDate
+        self.avatar = avatar
+        self.gameSessions = gameSessions
+
+    def get_id(self):
+        return self.id
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+
+def create_record(data):
+    date_now = datetime.now()
+    now_month = date_now.month
+    now_day = date_now.day
+
+    if now_month <= 10:
+        now_month = f"0{now_month}"
+
+    if now_day <= 10:
+        now_day = f"0{now_day}"
+
+    collection.insert_one({
+        'id': str(uuid.uuid4()),
+        'name': data['name'],
+        'email': data['email'],
+        'password': data['password'],
+        'registerDate': f"{now_day}.{now_month}.{date_now.year}",
+        'avatar': 'user_tmp_example.png',
+        'gameSessions': []
+    })
+
+
+def find_user_by_id(user_id):
+    cur_user = collection.find_one({'id': user_id})
+    if cur_user is not None:
+        return User(id=cur_user['id'],
+                    name=cur_user['name'],
+                    email=cur_user['email'],
+                    password=cur_user['password'],
+                    registerDate=cur_user['registerDate'],
+                    avatar=cur_user['avatar'],
+                    gameSessions=cur_user['gameSessions'])
+    else:
+        return None
+
+
+def find_user_by_email(value):
+    cur_user = collection.find_one({'email': value})
+    if cur_user is not None:
+        return User(id=cur_user['id'],
+                    name=cur_user['name'],
+                    email=cur_user['email'],
+                    password=cur_user['password'],
+                    registerDate=cur_user['registerDate'],
+                    avatar=cur_user['avatar'],
+                    gameSessions=cur_user['gameSessions'])
+    else:
+        return None
+
+
+def delete_record_by_id(value):
+    collection.delete_one({
+        'id': value
+    })
+
+
+def update_record(findKey, findValue, key, value):
+    collection.find_one_and_update({findKey: findValue},
+                                   {'$set': {key: value}})
+
+
+def update_and_push(findKey, findValue, key, value):
+    collection.find_one_and_update({findKey: findValue},
+                                   {'$push': {key: value}})
 
 if __name__ == '__main__':
     app.run(debug=False)
